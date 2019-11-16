a0e5988b93e5cfca40f4b1db9a95beca5e7bdbed
Sergei Shinn <55379976+Domodomko@users.noreply.github.com>
2019-11-16 22:58:37 +0600

    Update views.py

--- main/views.py
+++ main/views.py
@@ -2,30 +2,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<style>
-                           li {
-                            list-style-type: none;
-                            line-height: 1.8;
-                           }
-                           ul {
-                            margin-left: 0;
-                            padding-left: 0;
-                           }
-                           a {
-                            text-decoration: none;
-                            color: white;
-                           }
-                           a:hover {
-                            text-decoration: inherit;
-                            color: white;
-                           }
-                        </style>
-                        QUACK!
-                        <ul>
-                        <li><a href="/admin">/admin</a> - admin panel;</li>
-                        <li><a href="/courses/all">/courses/all</a> - all courses;</li>
-                        <li><a href="question.html">???</a></li>
-                        <li><a style="color:black;" href="black.html">Sans</a></li>
+    return HttpResponse("QUACK!
                         <p><b>/courses/all</b> - all courses;</p>
                         <p><b>/courses/createcat</b> - create category;</p>
                         <p><b>/courses/createbr</b> - create branch;</p>
