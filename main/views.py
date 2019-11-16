3feebf143bc7e298e280b124e1a3ecf7b7898451
Sergei Shinn <55379976+Domodomko@users.noreply.github.com>
2019-11-16 22:47:12 +0600

    Update views.py

--- main/views.py
+++ main/views.py
@@ -2,9 +2,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("QUACK!<p>/admin - admin panel;
-                        <p><b>/courses/all</b> - all courses;
-                        <p><b>/courses/createcat</b> - create category;
-                        <p><b>/courses/createbr</b> - create branch;
-                        <p><b>/courses/createcon</b> - create contact;
-                        <p><b>/courses/createcou</b> - create course.")
+    return HttpResponse("QUACK!
+                        <p><b>/admin</b> - admin panel;</p>
+                        <p><b>/courses/all</b> - all courses;</p>
+                        <p><b>/courses/createcat</b> - create category;</p>
+                        <p><b>/courses/createbr</b> - create branch;</p>
+                        <p><b>/courses/createcon</b> - create contact;</p>
+                        <p><b>/courses/createcou</b> - create course.</p>")
